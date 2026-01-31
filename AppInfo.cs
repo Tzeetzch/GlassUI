03f5366c2407c630af4a93a23ae8515e582ed19c
peter <p.kruithof@nitea.nl>
2026-01-31 12:26:04 +0100

    Move AppInfo version class to AppInfo.cs for proper visibility

---
+++ AppInfo.cs
@@ -0,0 +1,7 @@
+namespace GlassUI
+{
+    public static class AppInfo
+    {
+        public const string Version = "v1.1.0";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-namespace GlassUI
-{
-	public static class AppInfo
-	{
-		public const string Version = "v1.1.0";
-	}
-}
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
